e97c80aead6a74bc49dddb5080d6dc4ed8239af2
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-04-18 15:35:25 +0530

    refactor: removes unnecessary files

--- apps/product-hunt/pages/_app.tsx
+++ apps/product-hunt/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,15 +8,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to product-hunt!</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to product-hunt!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </>
   );
 }
